b270a61b6b3042cda65e5b93450a697ab91ceae5
Thomas van der Berg <thomas.van.der.berg.gm@gmail.com>
2014-03-10 11:43:21 +0100

    Added Image object

---
+++ js/entities/image.js
@@ -0,0 +1,38 @@
+/* Image: draws an image */
+Image = me.Renderable.extend({
+    init: function(x, y, image, w, h) {
+        if (x == null) {
+            x = 0;
+        }
+        if (y == null) {
+            y = 0;
+        }
+        if (image == null) {
+            image = "bg";
+        }
+        if (w == null) {
+            w = 1;
+        }
+        if (h == null) {
+            h = 1;
+        }
+        this.parent(new me.Vector2d(x, y, w, h));
+
+        this.loadImage(image);
+        this.floating = true;
+
+    },
+
+    loadImage: function(image) {
+        this.image = me.loader.getImage(image);
+    },
+
+    update: function() {
+        return true;
+    },
+
+    draw: function(ctx) {
+        ctx.drawImage(this.image, this.pos.x, this.pos.y, this.width,
+                                                                  this.height);
+    }
+});

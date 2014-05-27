4e9bc167451833c0af05fca3764029a79bafc944
Thomas van der Berg <thomas.van.der.berg.gm@gmail.com>
2014-05-27 09:16:01 +0200

    Added new level 6 and set upgradeLevels to 1

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 /* Constants: contains constant values used in the game */
 Constants =
 {
-	upgradeLevels: 2,
+	upgradeLevels: 1, // feature that never made it in
     screenWidth: 1024,
     screenHeight: 768,
     resizeToBrowser: true, // resize canvas to browser size 
